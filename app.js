a6fb622cf42a061c8f0d7362c0b98cd2f0217d9b
LuizGamaDev <luiz.fillipe3@hotmail.com>
2025-01-02 02:59:42 +0000

    feat: VueJS Installation

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-
-const { createApp } = Vue;
+const { createApp } = Vue
 
 createApp({
   data() {
     return {
-      message: "Hello Vue 3!",
-    };
-  },
-}).mount("#myapp");
+      message: 'Hello Vue!'
+    }
+  }
+}).mount('#myapp')
